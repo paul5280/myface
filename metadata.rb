69a7c03cdf40a0a0e1fa5323d6fdb26a817af659
Paul Brown <Paul.Brown@nordstrom.com>
2013-06-18 11:35:59 -0700

    Full working cookbook

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'paul5280-myface'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures paul5280-myface'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
